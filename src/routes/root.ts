d3fe67ffd7acf66773930467efe1e2a20b3a8c94
Faraz Patankar <farazpatankar@gmail.com>
2024-02-27 17:08:57 +0400

    Initial commit

---
+++ src/routes/root.ts
@@ -0,0 +1,12 @@
+import { FastifyPluginAsync } from 'fastify'
+
+const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.get('/', async function (request, reply) {
+    return { root: true }
+  })
+  fastify.get('/health', async function (request, reply) {
+    return reply.send("OK")
+  })
+}
+
+export default root;
