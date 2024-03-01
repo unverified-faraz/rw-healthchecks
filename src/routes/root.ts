1681050bb8566581bd2528db4ca036f5e7d5d40b
unverified-faraz <faraz@railway.app>
2024-03-01 09:43:09 +0400

    update response

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,7 +2,7 @@ import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
-    return { root: true, branch: "main" }
+    return { root: true, branch: "updated-main" }
   })
   fastify.get('/health', async function (request, reply) {
     return reply.send("OK")
